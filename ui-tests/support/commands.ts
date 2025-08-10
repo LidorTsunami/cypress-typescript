513dddfd6b7783a9d7d2bd0dd74113a9b8899d22
LidorTsunami <118756066+LidorTsunami@users.noreply.github.com>
2025-08-10 20:07:49 +0300

    refactored

--- ui-tests/support/commands.ts
+++ ui-tests/support/commands.ts
@@ -4,4 +4,12 @@ export function handleUncaughtExceptions(): void {
     Cypress.on('uncaught:exception', (err: Error, runnable: Runnable) => {
         return !err.message.includes('b.cardModuleFactory is not a function');
     });
+
+    if (typeof window !== 'undefined' && typeof window.KeyboardEvent === 'undefined') {
+        // @ts-ignore
+        window.KeyboardEvent = class KeyboardEvent {
+            constructor(type: string, eventInitDict?: KeyboardEventInit) {
+            }
+        };
+    }
 }
