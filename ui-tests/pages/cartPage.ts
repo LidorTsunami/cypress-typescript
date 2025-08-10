430c9623624eaec195516d0c9f302e7a4daae5a9
LidorTsunami <118756066+LidorTsunami@users.noreply.github.com>
2025-08-10 20:05:31 +0300

    refactored

--- ui-tests/pages/cartPage.ts
+++ ui-tests/pages/cartPage.ts
@@ -1,19 +1,22 @@
 export class CartPage {
     private readonly cartLocator: string;
+    private readonly itemXPathTemplate: string;
 
     constructor() {
         this.cartLocator = '#activeCartViewForm';
+        this.itemXPathTemplate = `//*[@id="sc-active-cart"]//span[
+            (contains(@class, "a-truncate-full") or contains(@class, "a-offscreen"))
+            and contains(., "{WORD}")
+        ]`;
     }
 
     verifyItemsInCartByItemText(...texts: string[]): void {
-        cy.get(this.cartLocator)
-            .should('be.visible')
-            .invoke('text')
-            .then((elementText: string) => {
-                texts.forEach(expectedText => {
-                    cy.log(`Verifying presence of: ${expectedText}`);
-                    expect(elementText).to.include(expectedText);
-                });
-            });
+        texts.forEach(expectedText => {
+            const firstWord = expectedText.split(' ')[0];
+            const xpath = this.itemXPathTemplate.replace('{WORD}', firstWord);
+
+            cy.log(`Verifying item in cart (partial match): ${expectedText}`);
+            cy.xpath(xpath, { timeout: 10000 }).should('exist');
+        });
     }
 }
